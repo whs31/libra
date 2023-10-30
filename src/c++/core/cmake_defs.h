518a4ffd1dc9ba1667a8bbb9e7745389114243d3
whs31 <75864292+whs31@users.noreply.github.com>
2023-10-30 15:07:46 +0300

    Removed fmt and expected because of cxx 20 adopted on my workplace!

--- src/c++/core/cmake_defs.h
+++ src/c++/core/cmake_defs.h
@@ -9,21 +9,3 @@
 #else
   #define LIBRA_NO_QT
 #endif
-
-#if defined(CMAKE_LIBRA_NO_FMT)
-  #define LIBRA_NO_FMT
-#else
-  #define LIBRA_FMT
-#endif
-
-#if defined(CMAKE_LIBRA_NO_RANGE)
-  #define LIBRA_NO_RANGE
-#else
-  #define LIBRA_RANGE
-#endif
-
-#if defined(CMAKE_LIBRA_NO_EXPECTED)
-  #define LIBRA_NO_EXPECTED
-#else
-  #define LIBRA_EXPECTED
-#endif
--- src/c++/core/expected.h
+++ src/c++/core/expected.h
@@ -5,16 +5,6 @@
 #pragma once
 
 #include "cmake_defs.h"
-
-#if defined(LIBRA_EXPECTED)
-  #include <tl/expected.hpp>
-  using namespace tl;
-#else
-  #if __cplusplus > 202002L && __cpp_concepts >= 202002L
-    #include <expected>
-    using std::expected;
-  #else
-    #error "You are doing something wrong. Expected is available since c++23, and your CMAKE_CXX_STANDARD is lower. Use LIBRA_NO_TL_EXPECTED OFF :3"
-  #endif
-#endif
+#include <tl/expected.hpp>
+using namespace tl;
 
